93ebe672b38655ee622f547464585b8305f14ab4
Rafael Ribeiro da Silva <eu@rafaelrsilva.com>
2018-12-12 19:26:06 -0200

    Podspec and xcode project added.

---
+++ RRBrazilianState.podspec
@@ -0,0 +1,14 @@
+Pod::Spec.new do |s|
+
+  s.name          = "RRBrazilianState"
+  s.version       = "0.0.1"
+  s.summary       = "Enum containing Brazilian states and cities"
+  s.homepage      = "https://github.com/rafaelrsilva/RRBrazilianState"
+  s.license       = "MIT"
+  s.author        = { "Rafael Ribeiro da Silva" => "eu@rafaelrsilva.com" }
+  s.platform      = :ios, "11.0"
+  s.swift_version = "4.2"
+  s.source        = { :git => "https://github.com/rafaelrsilva/RRBrazilianState.git", :tag => "0.0.1" }
+  s.source_files  = "RRBrazilianState/**/*.{h,m,swift}"
+  
+end
---
+++ RRBrazilianState/RRBrazilianState/RRBrazilianState.h
@@ -0,0 +1,19 @@
+//
+//  RRBrazilianState.h
+//  RRBrazilianState
+//
+//  Created by Rafael on 12/12/18.
+//  Copyright © 2018 Rafael Ribeiro da Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RRBrazilianState.
+FOUNDATION_EXPORT double RRBrazilianStateVersionNumber;
+
+//! Project version string for RRBrazilianState.
+FOUNDATION_EXPORT const unsigned char RRBrazilianStateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RRBrazilianState/PublicHeader.h>
+
+
